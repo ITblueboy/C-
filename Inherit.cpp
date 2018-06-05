276a99a7d422c42d305bfa902734ae7df1a0a8d8
ITblueboy <18309208067@163.com>
2018-06-05 23:53:19 +0800

    继承

---
+++ Inherit.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Person
+{
+public:
+  Person(const char* name="",int id=0)
+    :_name(name)
+    ,_num(id)
+  {}
+  void display()
+  {
+    cout<<_name<<endl;
+    cout<<_num<<endl;
+  }
+protected:
+  string _name;
+  int _num;
+};
+
+class Student:public Person 
+{
+  public:
+    Student(const char* name,int id,int stunum)
+      :Person(name,id)
+      ,_num(stunum)
+  {}
+    void display()
+    {
+      cout<<"身份证号"<<Person::_num<<endl;
+      cout<<"姓名"<<Person::_name<<endl;
+      cout<<"学号"<<_num<<endl;
+    }
+  protected:
+    int _num;
+};
+
+void test()
+{
+  Student s("Paul",101,001);
+  s.display();
+}
+int main()
+{
+  test();
+  return 0;
+}
